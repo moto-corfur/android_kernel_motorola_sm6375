1b47fd92d3cae9f620567899a45e5567b816a659
arter97 <qkrwngud825@gmail.com>
2022-08-10 15:54:03 +0200

    qcacld-3.0: Defer HDD initialization and rely on userspace writing to /dev/wlan
    Wi-Fi HAL writes "ON" or "OFF" to /dev/wlan.
    
    Use this method to initialize hdd as it's a safer
    way to ensure both wlan_mac.bin and WCNSS_qcom_cfg.ini
    is ready to be read. This also eliminates the needs for
    horrible hack to read userspace file.
    
    Change-Id: I1e3cc5ac997a6f8383e5047a5b5edcc5b4db377b

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -16731,6 +16731,8 @@ static void __hdd_inform_wifi_off(void)
 	ucfg_blm_wifi_off(hdd_ctx->pdev);
 }
 
+int hdd_driver_load(void);
+
 static void hdd_inform_wifi_off(void)
 {
 	int ret;
@@ -16828,7 +16830,7 @@ static ssize_t wlan_hdd_state_ctrl_param_write(struct file *filp,
 			goto exit;
 		}
 	}
-	
+
 	if (!cds_is_driver_loaded() || cds_is_driver_recovering()) {
 		rc = wait_for_completion_timeout(&wlan_start_comp,
 				msecs_to_jiffies(HDD_WLAN_START_WAIT_TIME));
